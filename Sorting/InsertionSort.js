90d03e7c8693962c316a505f07d30c3b69795bd9
Kiichi Sugihara <kiichi.sugihara@gmail.com>
2020-01-09 21:45:03 +0900

    add (sorting):  add InsertionSort

--- Sorting/InsertionSort.js
+++ Sorting/InsertionSort.js
@@ -1,5 +1,16 @@
 function InsertionSort(array) {
-  let a = [1, 2, 3, 4, 10];
-  return a;
+  for (let i = 1; i < array.length; i++) {
+    let tmp = array[i];
+    if (array[i - 1] > tmp) {
+      let j = i;
+
+      while (j > 0 && array[j - 1] > tmp) {
+        array[j] = array[j - 1];
+        j--;
+      }
+      array[j] = tmp;
+    }
+  }
+  return array;
 }
 module.exports = InsertionSort;
