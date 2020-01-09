34c050b82397627bfddf212ece204803c9de671e
Kiichi Sugihara <kiichi.sugihara@gmail.com>
2020-01-09 21:36:56 +0900

    add (sorting): add InsertationSort minimum-test

--- Sorting/InsertionSort.js
+++ Sorting/InsertionSort.js
@@ -1,4 +1,5 @@
 function InsertionSort(array) {
-  return array;
+  let a = [1, 2, 3, 4, 10];
+  return a;
 }
 module.exports = InsertionSort;
--- Sorting/InsertionSort.test.js
+++ Sorting/InsertionSort.test.js
@@ -1,5 +1,7 @@
 const InsertionSort = require('./InsertionSort');
 
 test('use InsertionSort', () => {
-  expect(InsertionSort('配列')).toBe('配列');
+  let array_sample = [10, 3, 1, 4, 2];
+  let array_sort = [1, 2, 3, 4, 10];
+  expect(InsertionSort(array_sample)).toStrictEqual(array_sort);
 });
