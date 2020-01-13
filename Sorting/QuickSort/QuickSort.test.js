42b807085a9a2476aca437539e941bdfe910a636
Kiichi Sugihara <kiichi.sugihara@gmail.com>
2020-01-13 16:04:56 +0900

    add (Sorting) : add QuickSort

--- Sorting/QuickSort/QuickSort.test.js
+++ Sorting/QuickSort/QuickSort.test.js
@@ -2,7 +2,8 @@ const QuickSort = require('./QuickSort');
 
 test('use QuickSort', () => {
   let array_sample = [10, 3, 1, 4, 2];
-  // let array_sort = [1, 2, 3, 4, 10];
-  let array_sort = [10, 3, 1, 4, 2];
-  expect(QuickSort(array_sample)).toStrictEqual(array_sort);
+  let array_sort = [1, 2, 3, 4, 10];
+  expect(QuickSort(array_sample, 0, array_sample.length - 1)).toStrictEqual(
+    array_sort
+  );
 });
