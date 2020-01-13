d1f49f222721f74d18a433dab22d514a9fd9c1bc
Kiichi Sugihara <kiichi.sugihara@gmail.com>
2020-01-13 15:32:56 +0900

    add (Sorting) : add MergeSort

--- Sorting/MergeSort/MergeSort.js
+++ Sorting/MergeSort/MergeSort.js
@@ -1,4 +1,29 @@
-function MergeSort(array) {
-  return array;
+function MergeSort(items) {
+  // 受け取ったdata(配列)が、最小単位になるまで分割し続ける。
+  if (items.length < 2) {
+    return items;
+  }
+
+  let middle = Math.floor(items.length / 2),
+    left = items.slice(0, middle),
+    right = items.slice(middle);
+
+  return merge(MergeSort(left), MergeSort(right));
+}
+function merge(left, right) {
+  let result = [],
+    il = 0,
+    ir = 0;
+
+  while (il < left.length && ir < right.length) {
+    if (left[il] < right[ir]) {
+      result.push(left[il++]);
+    } else {
+      result.push(right[ir++]);
+    }
+  }
+
+  // 配列で余った一番大きい値を最後にresultに入れて終了。
+  return result.concat(left.slice(il)).concat(right.slice(ir));
 }
 module.exports = MergeSort;
--- Sorting/MergeSort/MergeSort.test.js
+++ Sorting/MergeSort/MergeSort.test.js
@@ -2,7 +2,7 @@ const MergeSort = require('./MergeSort');
 
 test('use MergeSort', () => {
   let array_sample = [10, 3, 1, 4, 2];
-  // let array_sort = [1, 2, 3, 4, 10];
-  let array_sort = [10, 3, 1, 4, 2];
+  let array_sort = [1, 2, 3, 4, 10];
+  // let array_sort = [10, 3, 1, 4, 2];
   expect(MergeSort(array_sample)).toStrictEqual(array_sort);
 });
