8a5533c924be2e0912d56a3ed14db4f4ccb9cf76
Kiichi Sugihara <kiichi.sugihara@gmail.com>
2020-01-18 14:47:13 +0900

    add (Sorting): add HeapSort

--- Sorting/HeapSort/HeapSort.test.js
+++ Sorting/HeapSort/HeapSort.test.js
@@ -2,9 +2,6 @@ const heapSort = require('./HeapSort');
 
 test('use HeapSort', () => {
   let array_sample = [10, 3, 1, 4, 2];
-  // let array_sort = [1, 2, 3, 4, 10];
-  let array_sort = [10, 3, 1, 4, 2];
-  expect(heapSort(array_sample, 0, array_sample.length - 1)).toStrictEqual(
-    array_sort
-  );
+  let array_sort = [1, 2, 3, 4, 10];
+  expect(heapSort(array_sample)).toStrictEqual(array_sort);
 });
